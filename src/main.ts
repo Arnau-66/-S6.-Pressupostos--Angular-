42ad7a528b8a5f0f294f57ebb74d2a1ac3438afc
Arnau Perez <arnauperezesteve@gmail.com>
2025-08-26 12:34:21 +0200

    chore: add Bootstrap JS and configure locale es-ES in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs);
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers ?? []),
+    { provide: LOCALE_ID, useValue: 'es-ES' }
+  ]
+}).catch(err => console.error(err));
