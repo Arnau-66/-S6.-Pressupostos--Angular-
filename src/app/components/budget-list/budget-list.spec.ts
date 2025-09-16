7a71ed17386c02ec1c2a124daba1f572d26aaec4
Arnau Perez <arnauperezesteve@gmail.com>
2025-09-16 20:39:38 +0200

    Importing services to testing component

--- src/app/components/budget-list/budget-list.spec.ts
+++ src/app/components/budget-list/budget-list.spec.ts
@@ -1,16 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BudgetList } from './budget-list';
+import { BudgetService } from '../../services/budget';
 
 describe('BudgetList', () => {
   let component: BudgetList;
   let fixture: ComponentFixture<BudgetList>;
+  let service: BudgetService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BudgetList]
     })
     .compileComponents();
+
+    service = TestBed.inject(BudgetService);
+
+    service.addBudget({
+      client: { name: 'Ana', email: 'ana@mail.com', phone: '' },
+      services: { seo: true, ads: false, web: false, pages: 1, languages: 1 },
+      total: service.calculateTotalWithWebExtras({
+        seo: true, ads: false, web: false, pages: 1, languages: 1
+      })
+    } as any);
+
+    service.addBudget({
+      client: { name: 'Bruno', email: 'b@mail.com', phone: '' },
+      services: { seo: false, ads: true, web: true, pages: 2, languages: 2 },
+      total: service.calculateTotalWithWebExtras({
+        seo: false, ads: true, web: true, pages: 2, languages: 2
+      })
+    } as any);
 
     fixture = TestBed.createComponent(BudgetList);
     component = fixture.componentInstance;
@@ -20,4 +39,6 @@ describe('BudgetList', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  
 });
